c8318b4a12cad9023bd64bed2b215c611df42022
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-12-16 15:14:18 +0200

    компонент page404

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Page404() {
   return (
-    <div>Page404</div>
-  )
+    <div className="page-not-found">
+      <div className="page-not-found__container">
+        <h1 className="page-not-found__title">404</h1>
+        <p className="pege-not-found__subtitle">Страница не найдена</p>
+      </div>
+      <Link to="/" className="page-not-found__link">
+        Назад
+      </Link>
+    </div>
+  );
 }
